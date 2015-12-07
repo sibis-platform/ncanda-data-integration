e312871c76120b63de29e8cb2ab49035ce436ba5
Nolan Nichols <nolan.nichols@gmail.com>
2015-12-07 19:31:11 +0000

    add: new repo name

--- scripts/crond/crontools.sh
+++ scripts/crond/crontools.sh
@@ -23,9 +23,8 @@ catch_output_email()
     eval ${cmd} &> ${tmpfile}
     if [ -s ${tmpfile} ]; then
         eval "mailx -r crond@ncanda.sri.com -s \"${subject}\" ${mailto} < ${tmpfile}"
-        eval "python ${HOME}/scripts/crond/post_github_issues.py --org sibis-platform --repo ncanda-issues --title \"${subject}\" --body ${tmpfile}"
+        eval "python ${HOME}/scripts/crond/post_github_issues.py --org sibis-platform --repo ncanda-operations --title \"${subject}\" --body ${tmpfile}"
     fi
 
     rm -f ${tmpfile}
 }
-
