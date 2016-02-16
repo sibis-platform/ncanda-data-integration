cdb3156d037db1418de4a38f30003f50145af5ea
abonil91 <alfonso.bonilla@sri.com>
2016-02-16 15:36:26 -0800

    Update: Ncanda QC

--- scripts/reporting/ncanda_quality_control_script.py
+++ scripts/reporting/ncanda_quality_control_script.py
@@ -27,7 +27,25 @@ fields = ['study_id', 'redcap_event_name','exclude', 'visit_ignore',
           'youthreport2_date','parentreport_missing','parentreport_date',
           'ssage_youth_missing','ssage_youth_date', 'lssaga1_youth_missing',
           'lssaga1_youth_date','lssaga1_parent_missing','lssaga1_parent_date',
-          'mrireport_missing','mrireport_date','mr_session_report_complete'];
+          'bio_np_missing', 'bio_np_date','dd1000_missing','dd1000_date',
+          'dd100_missing','dd100_date','np_wrat4_missing','np_wrat4_wr_raw',
+          'np_gpeg_missing','np_gpeg_exclusion','np_gpeg_dh_time','np_gpeg_ndh_time',
+          'np_reyo_missing','np_reyo_copy_time', 'np_atax_missing','np_atax_sht_trial1',
+          'np_wais4_missing', 'np_wais4_corr15s', 'pasat_missing','pasat_date',
+          'cnp_missing','cnp_test_sessions_dotest','stroop_missing',
+          'stroop_date', 'mrireport_missing','mrireport_date',
+          'mr_session_report_complete'];
+
+form_fields = [['youthreport1_missing','youthreport1_date'],
+          ['youthreport1b_missing', 'youthreport1b_date'],['youthreport2_missing',
+          'youthreport2_date'],['parentreport_missing','parentreport_date'],
+          ['ssage_youth_missing','ssage_youth_date'],['lssaga1_youth_missing',
+          'lssaga1_youth_date'],['lssaga1_parent_missing','lssaga1_parent_date'],
+          ['bio_np_missing', 'bio_np_date'],['dd1000_missing','dd1000_date'],
+          ['dd100_missing','dd100_date'],['np_wrat4_missing','np_wrat4_wr_raw'],
+          ['np_reyo_missing','np_reyo_copy_time'], ['np_atax_missing','np_atax_sht_trial1'],
+          ['np_wais4_missing', 'np_wais4_corr15s'], ['pasat_missing','pasat_date'],
+          ['cnp_missing','cnp_test_sessions_dotest'],['stroop_missing','stroop_date']];
 
 def get_project_entry(args=None):
     """
@@ -53,30 +71,25 @@ def data_entry_fields(fields,project,arm):
         events=[arm])
     return data_entry_raw
 
-def exclude_visit_ignore_check(idx,row):
-    #exclude with a value of 1 is excluded
-    if math.isnan(row.get('exclude')):
-        # visit_ignore____yes with value 1 is ignored
-        if row.get('visit_ignore___yes') != 1:
-            return (idx, row)
-
 def missing_form(idx,row,field_missing, field_value):
     """
     Generate a report indicating which Questionnaires and Forms have not been
     entered
     """
     error = dict()
-    idx, row = exclude_visit_ignore_check(idx,row)
-    # form is not missing if form_missing if value nan or zero
-    if row.get(field_missing) != 1:
-        # for form_date, date is stored as a string
-        if type(row.get(field_value)) == float:
-            error = dict(subject_site_id = idx[0],
-                        visit_date = row.get('visit_date'),
-                        form_missing = field_missing,
-                        event_name = idx[1],
-                        visit_notes = row.get('visit_notes'),
-                        error = 'ERROR: Form is missing')
+    #exclude with a value of 1 is excluded
+    if math.isnan(row.get('exclude')):
+        # visit_ignore____yes with value 1 is ignored
+        if row.get('visit_ignore___yes') != 1:
+            # form is not missing if form_missing if value nan or zero
+            if row.get(field_missing) != 1:
+                # for form_date, date is stored as a string
+                if type(row.get(field_value)) == float:
+                    error = dict(subject_site_id = idx[0],
+                            visit_date = row.get('visit_date'),
+                            form_missing = field_missing,
+                            event_name = idx[1],
+                            error = 'ERROR: Form is missing')
     return error
 
 def fourteen_days_MRI_report(idx,row):
@@ -84,14 +97,17 @@ def fourteen_days_MRI_report(idx,row):
     Generate a report indicating which MRI reports have no data after 14 days.
     """
     error = dict()
-    idx, row = exclude_visit_ignore_check(idx,row)
-    if row.get('mrireport_missing') != 1:
-        if type(row.get('mrireport_missing')) == str:
-            if datetime.datetime.strptime(row.get('mrireport_date'),'%Y-%m-%d') == datetime.date.today()-datetime.timedelta(days = 14):
-                error = dict(subject_site_id = idx[0],
-                            visit_date = row.get('visit_date'),
-                            event_name = idx[1],
-                            error = 'ERROR: No MRI data after 14 days')
+    #exclude with a value of 1 is excluded
+    if math.isnan(row.get('exclude')):
+        # visit_ignore____yes with value 1 is ignored
+        if row.get('visit_ignore___yes') != 1:
+                    if row.get('mrireport_missing') != 1:
+                        if type(row.get('mrireport_missing')) == str:
+                            if datetime.datetime.strptime(row.get('mrireport_date'),'%Y-%m-%d') == datetime.date.today()-datetime.timedelta(days = 14):
+                                error = dict(subject_site_id = idx[0],
+                                visit_date = row.get('visit_date'),
+                                event_name = idx[1],
+                                error = 'ERROR: No MRI data after 14 days')
     return error
 
 def main(args=None):
@@ -100,9 +116,10 @@ def main(args=None):
     error = []
 
     for idx, row in project_df.iterrows():
-        check = missing_form(idx,row)
-        if check:
-            error.append(check)
+		for f in form_fields:
+			check = missing_form(idx,row,f[0],f[1])
+			if check:
+				error.append(check)
 
     for e in error:
         if e != 'null':
