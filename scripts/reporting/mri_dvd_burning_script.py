decd12fda38e491c34bc7a3db27f542447e1cfd1
abonil91 <alfonso.bonilla@sri.com>
2016-03-07 14:17:34 -0800

    ADD: eid_generation

--- scripts/reporting/mri_dvd_burning_script.py
+++ scripts/reporting/mri_dvd_burning_script.py
@@ -18,6 +18,7 @@ import csv
 
 import redcap
 import pandas as pd
+import shutil
 
 fields = ['study_id', 'redcap_event_name','exclude', 'visit_ignore',
           'visit_date','mri_missing', 'mri_xnat_sid','mri_series_t1',
@@ -52,6 +53,12 @@ def data_entry_fields(fields,project,arm):
 	data_entry_raw = project.export_records(fields=fields, forms = forms, format='df', events=[arm])
 	return data_entry_raw
 
+def get_session_scan(scan_field):
+    session_scan = []
+    for i in scan_field:
+        session_scan.append(i.split('/'))
+    return session_scan
+
 def main(args):
     project_entry = get_project_entry()
     project_df = data_entry_fields(fields,project_entry,args.visit)
@@ -63,9 +70,23 @@ def main(args):
         for i in reader:
             subject_list.append(i[0])
 
-    #Filter
+    # Filter
     filter_df = project_df[project_df['mri_xnat_sid'].isin(subject_list)]
-    print filter_df['mri_series_t1']
+
+    # Make eid text file
+    session_scan_t1 = get_session_scan(filter_df['mri_series_t1'])
+    session_scan_t2 = get_session_scan(filter_df['mri_series_t2'])
+
+    eids = []
+    for i in session_scan_t1:
+        eids.append(i[0])
+
+    for i in session_scan_t2:
+            eids.append(i[0])
+
+    eids = set(eids)
+    with open("{}/eidlist.txt".format(csv_dir), 'w') as file:
+        file.write("\n".join(eids))
 
 if __name__ == '__main__':
     import argparse
