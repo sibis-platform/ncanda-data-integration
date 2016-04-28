9d444269dd2d82cbfa680bddcec8e63e295dcb75
abonil91 <alfonso.bonilla@sri.com>
2016-04-28 13:42:16 -0700

    update: csv file generation

--- scripts/reporting/ncanda_quality_control_script.py
+++ scripts/reporting/ncanda_quality_control_script.py
@@ -301,9 +301,10 @@ def main(args):
         for e in error:
             if e == 'null':
                 error.remove(e)
-        f = csv.writer(open(args.csvdir, "wb+"))
-        f.writerow(["subject_site_id", "visit_date", "event_name", "error"])
-        for x in error:
+        with open(args.csvdir, 'wb+') as fi:
+            f = csv.writer(fi)
+            f.writerow(["subject_site_id", "visit_date", "event_name", "error"])
+            for x in error:
                f.writerow([x["subject_site_id"],
                            x["visit_date"],
                            x["event_name"],
