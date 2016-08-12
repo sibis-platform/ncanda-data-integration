236c163f169b6c72e5111b671a92752a4740760d
sbenito <benito.arce.sara@gmail.com>
2016-08-12 09:22:56 -0700

    make experiment JSON serializable by casting to string

--- scripts/xnat/sibis.py
+++ scripts/xnat/sibis.py
@@ -7,8 +7,4 @@ def logging(id, message, **kwargs):
     log = dict(experiment_site_id=id,
                error=message)
     log.update(kwargs)
-    try:
-        print(json.dumps(log, sort_keys=True))
-    except Exception as e:
-        print("ERROR: Failed to serialize to JSON: %s" % e)
-        print("Original log message: %s" % log)
+    print(json.dumps(log, sort_keys=True))
