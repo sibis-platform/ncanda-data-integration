44a2cdec0c2426814e4256d0daf32247e430db01
Nolan Nichols <nolan.nichols@gmail.com>
2016-09-07 17:34:26 -0700

    add: try/except for checking gradient files

--- scripts/xnat/make_session_niftis.py
+++ scripts/xnat/make_session_niftis.py
@@ -141,8 +141,15 @@ def export_to_nifti(interface, project, subject, session, session_label,
                                              '%s_%s' % (
                                                  scan,
                                                  scantype), 'image*.nii.xml'))
-                            case_gradients = cgt.get_all_gradients(
-                                xml_file_list, decimals=3)
+                            try:
+                                case_gradients = cgt.get_all_gradients(
+                                    xml_file_list, decimals=3)
+                            except AttributeError as error:
+                                sibis.logging(
+                                    session_label,
+                                    "Error: parsing XML files failed.",
+                                    xml_file_list=xml_file_list,
+                                    error=error)
                             errors = list()
                             if len(case_gradients) == len(gradients):
                                 for idx, frame in enumerate(case_gradients):
