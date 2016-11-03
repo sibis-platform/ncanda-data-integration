8113b527031b4ad30d593b1e5b1321059d72647a
Sara Benito Arce <benito.arce.sara@gmail.com>
2016-11-03 15:25:49 -0700

    Update sibis.py

--- scripts/redcap/sibis.py
+++ scripts/redcap/sibis.py
@@ -6,10 +6,5 @@ import json
 def logging(id, message, **kwargs):
     log = dict(experiment_site_id=id,
                error=message)
-    log.update(kwargs)
-    try:  
-      print(json.dumps(log, sort_keys=True))
-
-    except e: 
-      print "ERROR: redcap/sibis.py: logging: " + id + "  ===== message: " + message + " ====== kwargs" + str(kwargs)   
-
+    log.update(kwargs)  
+    print(json.dumps(log, sort_keys=True))
