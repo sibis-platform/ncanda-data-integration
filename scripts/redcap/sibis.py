82b10b9fbedb0d21dc04287c73c952b7321dc3dc
Sara Benito <benito.arce.sara@gmail.com>
2016-11-11 10:47:39 -0800

    Used sibisBeta module for logs in update_bulk_forms

--- scripts/redcap/sibis.py
+++ scripts/redcap/sibis.py
@@ -6,5 +6,5 @@ import json
 def logging(id, message, **kwargs):
     log = dict(experiment_site_id=id,
                error=message)
-    log.update(kwargs)  
+    log.update(kwargs)
     print(json.dumps(log, sort_keys=True))
