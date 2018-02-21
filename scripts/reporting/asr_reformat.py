a1691a3362887c3ade247248661559c83b0f5fb2
Simon Podhajsky <simon.podhajsky@gmail.com>
2018-02-21 14:51:40 -0800

    Copy-and-paste changes in CBC to ASR and YSR
    This tests the generalizabilty of CBC (per #272) and will be refactored
    in a later commit.

--- scripts/reporting/asr_reformat.py
+++ scripts/reporting/asr_reformat.py
@@ -1,56 +1,76 @@
 import time
-start = time.time()
-import redcap
-import array
-import string
-import csv
-import numpy
-import sys
-import glob
-from datetime import datetime
-import numpy
-from numpy import genfromtxt
-import numpy as np
-import pandas
-from pandas import DataFrame
+import redcap as rc
+import pandas as pd
+import os
+
+api_url = 'https://ncanda.sri.com/redcap/api/'
+api_key_entry = os.environ['REDCAP_API_KEY']
+project_entry = rc.Project(api_url, api_key_entry)
+
+additional_columns = ['study_id', 'redcap_event_name',
+                      'dob',  # 'sex',
+                      'age', 'mri_xnat_sid']
+# note that we don't actually need DOB (since age is calculated for each visit,
+# or sex (because we extract it from study_id)
+general_df = project_entry.export_records(fields=additional_columns,
+                                          format='df')
+general_df = general_df.round({'age': 0}).dropna(subset=['age'])
+general_df['age'] = general_df['age'].astype(int)
 
 
-api_url='https://ncanda.sri.com/redcap/api/'
-api_key_entry='Token'
-project_entry = redcap.Project(api_url, api_key_entry)
+asr_df = project_entry.export_records(fields=['study_id'],
+                                      forms=['youth_report_1b'], format='df')
+# filter columns so that they start with youthreport1_asr_section
+# only keep rows that have at least 100 responses
+asr_df_answers = (asr_df.filter(regex=r'^youthreport1_asr_section')
+                  .dropna(axis=0, how='any', thresh=100)
+                  .fillna(value=9))
 
-colnames2score=list()
-namelist=['1_asr01','1_asr02','1_asr03','1_asr04','1_asr05','1_asr06','1_asr07','2_asr08','2_asr09','2_asr10','2_asr11','2_asr12','2_asr13',
-          '2_asr14','3_asr15','3_asr16','3_asr17','3_asr18','3_asr19','3_asr20','3_asr21','4_asr22','4_asr23','4_asr24','4_asr25','4_asr26',
-          '4_asr27','4_asr28','5_asr29','5_asr30','5_asr31','5_asr32','5_asr33','5_asr34','5_asr35','6_asr36','6_asr37','6_asr38','6_asr39',
-          '6_asr40','6_asr41','6_asr42','7_asr43','7_asr44','7_asr45','7_asr46','7_asr47','7_asr48','7_asr49','8_asr50','8_asr51','8_asr52',
-          '8_asr53','8_asr54','8_asr55','9_asr56a','9_asr56b','9_asr56c','9_asr56d','9_asr56e','9_asr56f','9_asr56g','9_asr56h','9_asr56i',
-          '10_asr57','10_asr58','10_asr59','10_asr60','10_asr61','10_asr62','10_asr63','11_asr64','11_asr65','11_asr66','11_asr67','11_asr68',
-          '11_asr69','11_asr70','12_asr71','12_asr72','12_asr73','12_asr74','12_asr75','12_asr76','12_asr77','13_asr78','13_asr79','13_asr80',
-          '13_asr81','13_asr82','13_asr83','13_asr84','14_asr85','14_asr86','14_asr87','14_asr88','14_asr89','14_asr90','14_asr91','15_asr92',
-          '15_asr93','15_asr94','15_asr95','15_asr96','15_asr97','15_asr98','16_asr99','16_as100','16_as101','16_as102','16_as103','16_as104',
-          '16_as105','17_asr106','17_asr107','17_asr108','17_asr109','17_asr110','17_asr111','17_asr112','18_asr113','18_asr114','18_asr115',
-          '18_asr116','18_asr117','18_asr118','18_asr119','19_asr120','19_asr121','19_asr122','19_asr123']
+# shrink into a single column
+asr_df_answers['bpitems'] = (asr_df_answers
+                             .iloc[:, 0:(len(asr_df_answers.columns))]
+                             .astype(int)
+                             .astype(str)
+                             .apply(lambda x: "'" + ''.join(x), axis=1))
 
+asr_df_bpitems = asr_df_answers.loc[:, ['bpitems']]
 
+# with NaN-containing answers filtered out, grab age and XNAT SID for the
+# surviving records
+output_df = pd.merge(general_df, asr_df_bpitems,
+                     left_index=True, right_index=True,
+                     how='inner')
 
-for i in namelist:
-    colnames2score.append('youthreport1_asr_section'+str(i))
-colnames_score=['study_id','redcap_event_name','redcap_data_access_group','dob','sex','age','mri_xnat_sid']+colnames2score
-data_entry_raw= DataFrame(project_entry.export_records(fields=colnames_score))
-data_entry_raw['age']=data_entry_raw['age'].astype(str)
-data2score=data_entry_raw[(data_entry_raw["youthreport1_asr_section19_asr123"]!="")]
-data2score=data2score[(data2score["age"]!="")]
-data2score=data2score.loc[::,]
-print data2score.shape
-# # of subjects to be scored
-subject_count=data2score.shape[0]
+# Using the fact that pandas will automatically create an index as
+# range(nrows), we obtain subjectno instead of creating it ourselves.
+# - maybe it would be more pythonic to just assign range(n) to the column.
+output_df = output_df.reset_index()
+output_df.index.rename('subjectno', inplace=True)
+output_df = output_df.reset_index()  # get subjectno as a column
 
+# Assign contant values
+output_df = output_df.assign(admver=9.1,
+                             datatype='raw',
+                             dfo='//',
+                             formver='2003',
+                             dataver='2003',
+                             formno='9',
+                             formid='9',
+                             type='ASR',
+                             enterdate=time.strftime("%m/%d/%Y"),
+                             compitems="'" + ('9' * 36)
+                            )
 
-today=time.strftime("%m%d%Y")
-myfile_name='asr_reformate_'+today+'.csv'
+# Extract gender from study ID
+output_df = output_df.assign(gender=lambda x: x.study_id.str.extract(r'([MF])-[0-9]$'))
 
-header_name=["admver","datatype","subjectno","id","firstname","middlename","lastname","othername","gender","dob",
+# Rename columns to be reused
+output_df = output_df.rename(columns={'study_id': 'firstname',
+                                      'mri_xnat_sid': 'middlename',
+                                      'redcap_event_name': 'lastname'})
+
+# Rearrange existing columns and fill the non-existent one with NaNs
+adm_headers=["admver","datatype","subjectno","id","firstname","middlename","lastname","othername","gender","dob",
              "ethniccode","formver","dataver","formno","formid","type","enterdate","dfo","age","agemonths",
              "educcode","fobcode","fobgender","fparentses","fsubjses","fspouseses","agencycode","clincode",
              "bpitems","compitems","afitems","otheritems","experience","scafitems","facilityco","numchild",
@@ -66,47 +86,5 @@ header_name=["admver","datatype","subjectno","id","firstname","middlename","last
              "sped4","sped5","sped6","sped7","sped8","sped9","sped10","sped10a","sped10b","sped10c","admcatlg",
              "society","cethnic","ceduc","cfob","cagency","cclin","cintervr","crater","cfacilit","ctime","ctype",
              "cschool","cfudef1","cfudef2","csudef1"]
-
-
-print "Please to remember to fill in the cut off date"
-cut_off_date=datetime.strptime('2014-11-20 12:57:42', "%Y-%m-%d %H:%M:%S")
-asr_reformat=DataFrame(columns=header_name)
-#ysr_reformat["subjectno"]=range(1,subject_count+1)
-asr_reformat["subjectno"]=range(subject_count)
-asr_reformat["admver"]=9.1
-asr_reformat["datatype"]="raw"
-asr_reformat["age"]=data2score["age"].tolist()
-asr_reformat["firstname"]=data2score["study_id"].values
-asr_reformat["middlename"]=data2score["mri_xnat_sid"].values
-asr_reformat["lastname"]=data2score["redcap_event_name"].values
-asr_reformat["dob"]=data2score["dob"].values
-asr_reformat["dfo"]="//"
-asr_reformat["formver"]="2003"
-asr_reformat["dataver"]="2003"
-asr_reformat["formno"]="9"
-asr_reformat["formid"]="9"
-asr_reformat["type"]="ASR"
-asr_reformat["enterdate"]=time.strftime("%m/%d/%Y")
-asr_reformat["compitems"]="'999999999999999999999999999999999999"
-asr_reformat["bpitems"]="'"
-
-for i in range(subject_count):
-#for i in range(20):    
-    asr_reformat["gender"][i]=asr_reformat["firstname"][i][8]
-    record=[x.encode('UTF8') for x in list(data2score[colnames2score].values[i])]
-    asr_reformat["age"][i]=int(float(asr_reformat['age'][i]))
-    for j in range(len(namelist)):
-        asr_reformat["bpitems"][i]=asr_reformat["bpitems"][i]+record[j][1]
-##    print asr_reformat["bpitems"][i]
-##    print ("--------------------------")
-    print i
-
-
-myfile = open(myfile_name, 'w')
-    
-
-
-asr_reformat.to_csv(myfile_name, index=False)
-myfile.close()        
-elapsed = (time.time() - start)
-print elapsed
+output_df = output_df.reindex(columns=adm_headers)
+output_df.to_csv('asr_prep_' + time.strftime("%m%d%Y") + '.csv', index=False)
--- scripts/reporting/ysr_reformat.py
+++ scripts/reporting/ysr_reformat.py
@@ -1,54 +1,75 @@
 import time
-start = time.time()
-import redcap
-import array
-import string
-import csv
-import numpy
-import sys
-import glob
-from datetime import datetime
-import numpy
-from numpy import genfromtxt
-import numpy as np
-import pandas
-from pandas import DataFrame
+import redcap as rc
+import pandas as pd
+import os
 
+api_url = 'https://ncanda.sri.com/redcap/api/'
+api_key_entry = os.environ['REDCAP_API_KEY']
+project_entry = rc.Project(api_url, api_key_entry)
 
-api_url='https://ncanda.sri.com/redcap/api/'
-api_key_entry='Token'
-project_entry = redcap.Project(api_url, api_key_entry)
+additional_columns = ['study_id', 'redcap_event_name',
+                      'dob',  # 'sex',
+                      'age', 'mri_xnat_sid']
+# note that we don't actually need DOB (since age is calculated for each visit) 
+# or sex (because we extract it from study_id)
+general_df = project_entry.export_records(fields=additional_columns, 
+                                          format='df')
+general_df = general_df.round({'age': 0}).dropna(subset=['age'])
+general_df['age'] = general_df['age'].astype(int)
 
-colnames2score=list()
-namelist=['1_ysr01','1_ysr02','1_ysr03','1_ysr04','1_ysr05','1_ysr06','1_ysr07','2_ysr08','2_ysr09','2_ysr10','2_ysr11','2_ysr12',
-          '2_ysr13','2_ysr14','3_ysr15','3_ysr16','3_ysr17','3_ysr18','3_ysr19','3_ysr20','3_ysr21','4_ysr22','4_ysr23','4_ysr24',
-          '4_ysr25','4_ysr26','4_ysr27','4_ysr28','5_ysr29','5_ysr30','5_ysr31','5_ysr32','5_ysr33','5_ysr34','5_ysr35','6_ysr36',
-          '6_ysr37','6_ysr38','6_ysr39','6_ysr40','6_ysr41','6_ysr42','7_ysr43','7_ysr44','7_ysr45','7_ysr46','7_ysr47','7_ysr48',
-          '7_ysr49','8_ysr50','8_ysr51','8_ysr52','8_ysr53','8_ysr54','8_ysr55','9_ysr56a','9_ysr56b','9_ysr56c','9_ysr56d','9_ysr56e',
-          '9_ysr56f','9_ysr56g','9_ysr56h','10_ysr57','10_ysr58','10_ysr59','10_ysr60','10_ysr61','10_ysr62','10_ysr63','11_ysr64',
-          '11_ysr65','11_ysr66','11_ysr67','11_ysr68','11_ysr69','11_ysr70','12_ysr71','12_ysr72','12_ysr73','12_ysr74','12_ysr75',
-          '12_ysr76','12_ysr77','13_ysr78','13_ysr79','13_ysr80','13_ysr81','13_ysr82','13_ysr83','13_ysr84','14_ysr85','14_ysr86',
-          '14_ysr87','14_ysr88','14_ysr89','14_ysr90','14_ysr91','15_ysr92','15_ysr93','15_ysr94','15_ysr95','15_ysr96','15_ysr97',
-          '15_ysr98','16_ysr99','16_ysr100','16_ysr101','16_ysr102','16_ysr103','16_ysr104','16_ysr105','17_ysr106','17_ysr107',
-          '17_ysr108','17_ysr109','17_ysr110','17_ysr111','17_ysr112']
+ysr_df = project_entry.export_records(fields=['study_id'],
+                                      forms=['youth_report_1b'], format='df')
+# filter columns so that they start with youthreport1_asr_section
+# only keep rows that have at least 100 responses
+ysr_df_answers = (ysr_df.filter(regex=r'^youthreport1_ysr_section')
+                  .dropna(axis=0, how='any', thresh=100)
+                  .fillna(value=9))
 
-for i in namelist:
-    colnames2score.append('youthreport1_ysr_section'+str(i))
-colnames_score=['study_id','redcap_event_name','redcap_data_access_group','dob','sex','age','mri_xnat_sid']+colnames2score
-data_entry_raw= DataFrame(project_entry.export_records(fields=colnames_score))
-data_entry_raw['age']=data_entry_raw['age'].astype(str)
-data2score=data_entry_raw[(data_entry_raw["youthreport1_ysr_section10_ysr57"]!="")]
-data2score=data2score[(data2score["age"]!="")]
-data2score=data2score.loc[::,]
-print data2score.shape
-# # of subjects to be scored
-subject_count=data2score.shape[0]
+# shrink into a single column
+ysr_df_answers['bpitems'] = (ysr_df_answers
+                             .iloc[:, 0:(len(ysr_df_answers.columns))]
+                             .astype(int)
+                             .astype(str)
+                             .apply(lambda x: "'" + ''.join(x), axis=1))
 
+ysr_df_bpitems = ysr_df_answers.loc[:, ['bpitems']]
 
-today=time.strftime("%m%d%Y")
-myfile_name='ysr_reformate_'+today+'.csv'
+# with NaN-containing answers filtered out, grab age and XNAT SID for the
+# surviving records
+output_df = pd.merge(general_df, ysr_df_bpitems, 
+                     left_index=True, right_index=True, 
+                     how='inner')
 
-header_name=["admver","datatype","subjectno","id","firstname","middlename","lastname","othername","gender","dob",
+# Using the fact that pandas will automatically create an index as
+# range(nrows), we obtain subjectno instead of creating it ourselves.
+# - maybe it would be more pythonic to just assign range(n) to the column.
+output_df = output_df.reset_index()
+output_df.index.rename('subjectno', inplace=True)
+output_df = output_df.reset_index()  # get subjectno as a column
+
+# Assign contant values
+output_df = output_df.assign(admver=9.1,
+                             datatype='raw',
+                             dfo='//',
+                             formver='2001',
+                             dataver='2001',
+                             formno='13',
+                             formid='13',
+                             type='YSR',
+                             enterdate=time.strftime("%m/%d/%Y"),
+                             compitems="'" + ('9' * 36)
+                            )
+
+# Extract gender from study ID
+output_df = output_df.assign(gender=lambda x: x.study_id.str.extract(r'([MF])-[0-9]$'))
+
+# Rename columns to be reused
+output_df = output_df.rename(columns={'study_id': 'firstname', 
+                                      'mri_xnat_sid': 'middlename', 
+                                      'redcap_event_name': 'lastname'})
+
+# Rearrange existing columns and fill the non-existent one with NaNs
+adm_headers=["admver","datatype","subjectno","id","firstname","middlename","lastname","othername","gender","dob",
              "ethniccode","formver","dataver","formno","formid","type","enterdate","dfo","age","agemonths",
              "educcode","fobcode","fobgender","fparentses","fsubjses","fspouseses","agencycode","clincode",
              "bpitems","compitems","afitems","otheritems","experience","scafitems","facilityco","numchild",
@@ -64,155 +85,5 @@ header_name=["admver","datatype","subjectno","id","firstname","middlename","last
              "sped4","sped5","sped6","sped7","sped8","sped9","sped10","sped10a","sped10b","sped10c","admcatlg",
              "society","cethnic","ceduc","cfob","cagency","cclin","cintervr","crater","cfacilit","ctime","ctype",
              "cschool","cfudef1","cfudef2","csudef1"]
-
-
-print "Please to remember to fill in the cut off date"
-cut_off_date=datetime.strptime('2014-11-20 12:57:42', "%Y-%m-%d %H:%M:%S")
-ysr_reformat=DataFrame(columns=header_name)
-#ysr_reformat["subjectno"]=range(1,subject_count+1)
-ysr_reformat["subjectno"]=range(subject_count)
-ysr_reformat["admver"]=9.1
-ysr_reformat["datatype"]="raw"
-#age=data2score["age"].astype(string)
-#aaa=[x.encode('UTF8') for x in age]
-##bbb=[x.split('.')[0] for x in aaa]
-##ysr_reformat["age"]=ysr_reformat["age"].round(decimals=0)
-#data2score["age1"]=data2score["age"].astype(str)
-ysr_reformat["age"]=data2score["age"].tolist()
-ysr_reformat["firstname"]=data2score["study_id"].values
-ysr_reformat["middlename"]=data2score["mri_xnat_sid"].values
-ysr_reformat["lastname"]=data2score["redcap_event_name"].values
-ysr_reformat["dob"]=data2score["dob"].values
-ysr_reformat["dfo"]="//"
-ysr_reformat["formver"]="2001"
-ysr_reformat["dataver"]="2001"
-ysr_reformat["formno"]="13"
-ysr_reformat["formid"]="13"
-ysr_reformat["type"]="YSR"
-ysr_reformat["enterdate"]=time.strftime("%m/%d/%Y")
-ysr_reformat["compitems"]="'999999999999999999999999999999999999"
-ysr_reformat["bpitems"]="'"
-
-for i in range(subject_count):
-#for i in range(20):    
-    ysr_reformat["gender"][i]=ysr_reformat["firstname"][i][8]
-    record=[x.encode('UTF8') for x in list(data2score[colnames2score].values[i])]
-    ysr_reformat["age"][i]=int(float(ysr_reformat['age'][i]))
-    #ysr_reformat["bpitems"]="'"
-    for j in range(len(namelist)):
-        ysr_reformat["bpitems"][i]=ysr_reformat["bpitems"][i]+record[j][1]
-##    print ysr_reformat["bpitems"][i]
-##    print ("--------------------------")
-    print i    
-
-
-myfile = open(myfile_name, 'w')
-    
-###read in all youth report
-##data=pandas.io.parsers.read_csv("C:/Users/wchu/Desktop/work/NCANDA ASEBA Scoring Tools/python/ysr2014-11-25.csv",header=0)
-##
-###select subjects only complete the report
-##data_complete=data[data['youth_report_1b_complete']==1]
-##data_complete=data_complete[data_complete['age'].round(decimals=1)>0]
-##
-###colums for asr scoringysr_reformat["i"][i]
-##colnames_id=['study_id','age','sex','redcap_data_access_group','redcap_event_name']
-##namelist=['1_ysr01','1_ysr02','1_ysr03','1_ysr04','1_ysr05','1_ysr06','1_ysr07','2_ysr08','2_ysr09','2_ysr10','2_ysr11','2_ysr12',
-##          '2_ysr13','2_ysr14','3_ysr15','3_ysr16','3_ysr17','3_ysr18','3_ysr19','3_ysr20','3_ysr21','4_ysr22','4_ysr23','4_ysr24',
-##          '4_ysr25','4_ysr26','4_ysr27','4_ysr28','5_ysr29','5_ysr30','5_ysr31','5_ysr32','5_ysr33','5_ysr34','5_ysr35','6_ysr36',
-##          '6_ysr37','6_ysr38','6_ysr39','6_ysr40','6_ysr41','6_ysr42','7_ysr43','7_ysr44','7_ysr45','7_ysr46','7_ysr47','7_ysr48',
-##          '7_ysr49','8_ysr50','8_ysr51','8_ysr52','8_ysr53','8_ysr54','8_ysr55','9_ysr56a','9_ysr56b','9_ysr56c','9_ysr56d','9_ysr56e',
-##          '9_ysr56f','9_ysr56g','9_ysr56h','10_ysr57','10_ysr58','10_ysr59','10_ysr60','10_ysr61','10_ysr62','10_ysr63','11_ysr64',
-##          '11_ysr65','11_ysr66','11_ysr67','11_ysr68','11_ysr69','11_ysr70','12_ysr71','12_ysr72','12_ysr73','12_ysr74','12_ysr75',
-##          '12_ysr76','12_ysr77','13_ysr78','13_ysr79','13_ysr80','13_ysr81','13_ysr82','13_ysr83','13_ysr84','14_ysr85','14_ysr86',
-##          '14_ysr87','14_ysr88','14_ysr89','14_ysr90','14_ysr91','15_ysr92','15_ysr93','15_ysr94','15_ysr95','15_ysr96','15_ysr97',
-##          '15_ysr98','16_ysr99','16_ysr100','16_ysr101','16_ysr102','16_ysr103','16_ysr104','16_ysr105','17_ysr106','17_ysr107',
-##          '17_ysr108','17_ysr109','17_ysr110','17_ysr111','17_ysr112']
-##
-##for i in namelist:
-##    colnames_score.append('youthreport1_ysr_section'+str(i))
-## 
-###get subjects info as well as item to scores    
-##data2subject=data_complete.loc[::,colnames_id]    
-##data2score=data_complete.loc[::,colnames_score]
-##
-##
-##
-###insert scoring type
-##data2subject.insert(0,column="type",value="|YSR", allow_duplicates=True)
-##data2subject.insert(1,column="Version",value="|2001", allow_duplicates=True)
-##data2subject.insert(7,column="Adaptive",value="99999999999999999999999999999999999   ", allow_duplicates=True)
-###data2subject['youthreport1b_age']=data2subject['youthreport1b_age'].round(decimals=0)
-###data2subject['youthreport1b_age']=data2subject['youthreport1b_age'].convert_objects(convert_numeric=True)
-##data2subject['age']=data2subject['age'].astype('int')
-##
-###replacing missing value with 9 according to ASR
-###data2score.fillna("9")
-##
-##
-###get dimention of the dataframe
-##dim=data2score.shape
-##total_rows=dim[0]
-##
-##for i in range(0,total_rows):
-###for i in range(0,4):
-##    #select ith record
-##    subject=data2subject.iloc[[i]]
-##    score=data2score.iloc[[i]]
-##    subject['study_id']=subject['study_id'] + ' '           #make subject_id to be 12 spaces
-##    subject['age']=subject['age'].astype('str') + ' '  #make age to be 3 spaces
-##    access_group=subject['redcap_data_access_group'].irow(0)+ '                '
-##    subject['redcap_data_access_group'] =access_group[0:15]
-##    event=subject['redcap_event_name'].irow(0)+'                     '
-##    subject['redcap_event_name']=event[0:20]
-##    #120 item need asr scores, convert to array
-##    subject=subject.reset_index(drop=True) 
-##    score = score.reset_index(drop=True)
-##    score_array =score.reset_index().values
-##    na_index=score_array.nonzero()
-##    #get gender from subject_id which is more accurant and complete
-##    if (subject['study_id'].loc[0].find("M"))>0:
-##         subject['sex'] = 1
-##    if (subject['study_id'].loc[0].find("F"))>0:
-##         subject['sex'] = 2     
-##    if (len(na_index[1]) < 118):
-##        #print(len(na_index[1]))
-##        subject_array=subject.reset_index(drop=True).values
-##        subject_list=subject_array.tolist()
-##        score_list=score_array.tolist()
-##        subject_list=subject_list[0]
-##        score_list=score_list[0]
-##        subject_out=subject_list[0]
-##        score_out=""
-##        for x in range(1,8) :
-##            if str(subject_list[x])=="nan":
-##                subject_out=subject_out+"9|"
-##            else:
-##                subject_out=subject_out+str(subject_list[x])+'|'
-##        for x in range(1,(len(score_list))) :
-##            if str(score_list[x])=="nan":
-##                #print(score_list)
-##                score_out=score_out+"9"
-##            else:
-##                #print str(int(score_list[x]))  
-##                score_out=score_out+str(int(score_list[x]))
-##        #tobacco=str(int(score_list[x+1]))+'    '
-##        #tobacco=tobacco[0:3]
-##        #drunk=str(int(score_list[x+2]))+'    '
-##        #drunk=tobacco[0:3]
-##        #drug=str(int(score_list[x+3]))+'    '
-##        #drug=tobacco[0:3]   
-##        #score_out=score_out+'   |'
-##        #score_out=score_out+tobacco+'|'+drunk+'|'+drug+'|'    
-##            #print(x)
-##        #subject_out=''.join(subject_array)
-##        #score_array=score_array.astype(int)
-####        aa.append([item_array[0,x] for x in range(0,dim[1])])
-##        #print ', '.join(subject_array)
-##        print>>myfile,subject_out+score_out
-##        #print>>myfile,score_out
-
-ysr_reformat.to_csv(myfile_name, index=False)
-myfile.close()        
-elapsed = (time.time() - start)
-print elapsed
+output_df = output_df.reindex(columns = adm_headers)
+output_df.to_csv('ysr_prep_' + time.strftime("%m%d%Y") + '.csv', index=False)
