74502e60362bd0ff2e57b77286543c69d5001c1a
Simon Podhajsky <simon.podhajsky@gmail.com>
2018-03-01 19:31:41 -0800

    Flag other invalid combos of content, missingness, and completeness

--- scripts/qc/QC_all.py
+++ scripts/qc/QC_all.py
@@ -56,6 +56,12 @@ forms_for_qc = get_forms_for_qc(api, qc_event, qc_arm)
 
 # In[ ]:
 
+forms_to_skip = ['biological_np', 'biological_mr', 'mr_session_report', 'mri_report']
+[forms_for_qc.remove(form) for form in forms_to_skip]
+
+
+# In[ ]:
+
 def get_data_for_event(rc_api, event, forms=None, arm=1):
     if forms is None:
         forms = get_forms_for_qc(rc_api, event, arm)
@@ -268,7 +274,7 @@ def get_items_matching_regex(regex, haystack):
 
 # In[ ]:
 
-def flag_incomplete_forms(df):
+def flag_potential_omissions_marked_complete(df):
     # some forms don't have it, some forms have it for multiple subsections
     cols_missing = get_items_matching_regex(r'missing$', df.columns)
     # all forms have it, some forms have it for multiple subsections
@@ -312,13 +318,13 @@ def flag_incomplete_forms(df):
 # In[ ]:
 
 # map(flag_incomplete_forms, np_data)
-np_flags = {form: flag_incomplete_forms(data_all[form]) for form in np_forms}
+np_flags = {form: flag_potential_omissions_marked_complete(data_all[form]) for form in np_forms}
 np_flags
 
 
 # In[ ]:
 
-all_flags = {form: flag_incomplete_forms(data_all[form]) for form in forms_for_qc}
+all_flags = {form: flag_potential_omissions_marked_complete(data_all[form]) for form in forms_for_qc}
 all_flags
 
 
@@ -378,4 +384,204 @@ check_form('participant_last_use_summary')
 # In[ ]:
 
 # flagged_data = {form: data_all[form].loc[flags] for form, flags in all_flags.values() if isinstance(flags, list)}
+
+
+# # Incomplete forms?
+# 
+# After talking to Devin, Ryan and Vanessa, I realized that the scope of this check was limited.
+# 
+# The previous check could best be characterized as "flagging records that were mistakenly labeled 'complete' when they were, in fact, missing information." This lets off the hook the records that were not marked missing and should have been.
+# 
+# But the interplay of `*_missing` and `*_complete` has slightly more complex rules / usage:
+# 
+# 1. If a form is marked missing, it should always be marked "complete". It might not, in practice, be marked that, but it should be.
+# 2. If a form isn't marked missing, it might be marked either:
+#     a. **incomplete** ("missing" in some data dictionaries, which is confusing): the data exists, but it hasn't been entered yet.
+#     b. **unverified**: the data has been entered, but should be re-checked,
+#     c. **complete**: the data as entered is currently assumed to be the complete record for that form.
+#    
+# So, essentially, there are five states a form can be in, and their failure modes are different:
+# 
+# 1. **Marked permanently missing**: should not have any data in it, and completion mark doesn't matter (but it _should_ be marked "complete"). Flag if:
+#     * Has data, or
+#     * Is not marked complete. (In theory, the script could auto-mark it complete, as there is no other resolution.)
+# 2. **Not missing but incomplete**: site should enter data. **This is the default status of a form.** Flag if:
+#     * The deadline for data collection has passed. (Data should either be filled in and form should be marked complete, or the form should be marked permanently missing and complete.)
+# 3. **Not missing but unverified**: site should do the second part of double entry. Flag if:
+#     * The deadline for data collection has passed.
+#     * _Maybe?_ The data has been entered into the form more than n days ago.
+# 4. **Not missing and complete**: site is done with the form (but might have overlooked something). Flag if:
+#     * Participant is missing some responses that other participants have. (This is the original logic of this PR.)
+# 5. **Not marked anything**: In theory, this shouldn't happen at all, so it should always be flagged.
+# 
+# The previous check doesn't bother with (1); it only addresses (4).
+# 
+# ## Checking for visit date?
+# This is where it might make sense to check if the participant has _any_ data for the year.
+# 
+# Then again, maybe not? Global collection status may not matter to a form.
+# 
+# ## Checking for "should this visit be ignored?"
+# In general, yes - e.g. exclusions at baseline will be marked that every year, and there's no way to tell just one form at a time.
+# 
+# (In the future, we might want to use specific exceptions for exclusion at baseline, but that doesn't change the case here.)
+
+# ## Issue with checkboxes
+# In some forms - biological NP, biological MR, maybe others? - checkboxes are used to solicit response. The default value that will be exported, **even if the form was not filled out**, is 0. Depending on whether the participant actually saw and filled out the form, the value `0` should be interpreted as either `0` (if seen) or `NaN` (if not seen).
+# 
+# This can be inferred from the presence of a date on _some_ forms, but not all. Here's a list of checkboxes that can be a problem:
+
+# In[ ]:
+
+forms_with_checkboxes = {
+    'np_grooved_pegboard': ['np_gpeg_exclusion___dh', 'np_gpeg_exclusion___ndh'],
+    'np_reyosterreith_complex_figure_files': ['np_reyo_ndh___yes'],
+    'np_wais4_coding': ['np_wais4_rawscore_diff___correct']
+}
+
+
+# ## Implementation
+
+# In[ ]:
+
+def get_missing_and_complete_column_names(df):
+    # some forms don't have it, some forms have it for multiple subsections
+    cols_missing = get_items_matching_regex(r'missing$', df.columns)
+    # all forms have it, some forms have it for multiple subsections
+    cols_complete = get_items_matching_regex("complete$", df.columns)
+    
+    return cols_missing, cols_complete
+
+
+# The good news is, there don't appear to be any data lost behind records labeled "missing". Of course, if REDCap wipes the data when the form is marked missing, that would be impossible.
+# 
+# False positives are due to checkboxes.
+
+# In[ ]:
+
+def flag_missing_with_data(df):
+    # 1. is it marked missing
+    # 2. does it have any fields that are filled out?
+    cols_missing, cols_complete = get_missing_and_complete_column_names(df)
+    
+    if len(cols_missing) == 1:
+        query_missing = "%s == 1" % cols_missing[0]
+    else:
+        return None
+    
+    df_relevant = df.query(query_missing)
+    if len(df_relevant) == 0:
+        return None
+
+    # Now, remove all complete / missing columns and stack
+    df_relevant = df_relevant.filter(regex=r'^((?!missing|complete).)*$').        stack(dropna=True).count(level=0)
+    return df_relevant[df_relevant > 0].index.tolist()
+
+{form: flag_missing_with_data(data_all[form]) for form in forms_for_qc}
+
+
+# In[ ]:
+
+def flag_missing_not_marked_complete(df):
+    # 1. is it flagged missing
+    # 2. is it not flagged complete
+    cols_missing, cols_complete = get_missing_and_complete_column_names(df)
+    query_not_complete = " or ".join(["%s == 2" % col for col in cols_complete])
+    
+    if len(cols_missing) == 1:
+        query_missing = "%s == 1" % cols_missing[0]
+        query_not_complete = "(" + query_not_complete + ") and " + query_missing
+    else:
+        return None
+ 
+    df_relevant = df.query(query_not_complete)
+    if len(df_relevant) == 0:
+        return None
+    return df_relevant.index.tolist()
+
+    # Now, remove all complete / missing columns and stack
+    df_relevant = df_relevant.filter(regex=r'^((?!missing|complete).)*$').        stack(dropna=False).count(level=0)
+    return df_relevant[df_relevant == 0].index.tolist()
+{form: flag_missing_not_marked_complete(data_all[form]) for form in forms_for_qc}
+
+
+# This is where checkboxes are causing a lot of false _negatives_. Since the checkbox reports in as 0, it appears as though there's data.
+# 
+# This is also the most common status.
+
+# In[ ]:
+
+def flag_incomplete_without_data(df):
+    # Equivalent of grey circle in REDCap.
+    #
+    # 1. is it marked incomplete
+    # 2. is it not marked missing
+    # 3. does it have any data
+    
+    # By default, forms are marked to receive more data, so this will return almost everything.
+    # If they haven't received data by collection deadline, should be marked missing+complete
+    cols_missing, cols_complete = get_missing_and_complete_column_names(df)
+    query_incomplete = " or ".join(["%s == 0" % col for col in cols_complete])
+    
+    if len(cols_missing) == 1:
+        query_present = "%s != 1" % cols_missing[0]
+        query_incomplete = "(" + query_incomplete + ") and " + query_present
+ 
+    df_relevant = df.query(query_incomplete)
+    if len(df_relevant) == 0:
+        return None
+
+    # Now, remove all complete / missing columns and stack
+    df_relevant = df_relevant.filter(regex=r'^((?!missing|complete).)*$').        stack(dropna=False).count(level=0)
+    return df_relevant[df_relevant == 0].index.tolist()
+
+{form: flag_incomplete_without_data(data_all[form]) for form in forms_for_qc}
+
+
+# The checkboxes are, again, causing a lot of false positives here: 
+
+# In[ ]:
+
+def flag_incomplete_with_data(df):
+    # This is the equivalent of a red circle in REDCap.
+    #
+    # 1. is it marked incomplete
+    # 2. is it not marked missing
+    # 3. does it have any data
+    #
+    # Follow-up: Site should work to fill in data, or mark record as complete
+    #  (or delete data if there was a mix-up)
+    
+    cols_missing, cols_complete = get_missing_and_complete_column_names(df)
+    query_incomplete = " or ".join(["%s == 0" % col for col in cols_complete])
+    
+    if len(cols_missing) == 1:
+        query_present = "%s != 1" % cols_missing[0]
+        query_incomplete = "(" + query_incomplete + ") and " + query_present
+ 
+    df_relevant = df.query(query_incomplete)
+    if len(df_relevant) == 0:
+        return None
+
+    # Now, remove all complete / missing columns and stack
+    df_relevant = df_relevant.filter(regex=r'^((?!missing|complete).)*$').        stack(dropna=True).count(level=0)
+    return df_relevant[df_relevant > 0].index.tolist()
+
+{form: flag_incomplete_with_data(data_all[form]) for form in forms_for_qc}
+
+
+# In[ ]:
+
+def flag_unverified_forms(df):
+    # These forms are marked to receive more data. 
+    # It doesn't matter whether they're marked missing or not; they should be verified.
+    
+    cols_missing, cols_complete = get_missing_and_complete_column_names(df)
+    query_unverified = " or ".join(["%s == 1" % col for col in cols_complete])
+        
+    df_relevant = df.query(query_unverified)
+    if len(df_relevant) == 0:
+        return None
+    return df_relevant.index.tolist()
+{form: flag_unverified_forms(data_all[form]) for form in forms_for_qc}
 
