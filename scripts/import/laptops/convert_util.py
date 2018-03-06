9ff02b544941f4740583a1d78ddb900d18a16183
kipohl <mphasak@gmail.com>
2018-03-06 11:00:27 -0800

    added options for calling script

--- scripts/import/laptops/convert_util.py
+++ scripts/import/laptops/convert_util.py
@@ -1,4 +1,5 @@
 import sys
+import hashlib
 from sibispy import sibislogger as slog
 
 # have to post issues that way so that github is not overwhelmed with calls 
@@ -6,7 +7,8 @@ def post_issue_and_exit(script, infile, verbose, post_to_github, issue_label, is
     slog.init_log(verbose, post_to_github,'NCANDA Import-Laptop: ' + script + ' Message', script)
 
     infoTxt="After this issue is resolved please run 'harvester --file-to-upload " + infile + " --overwrite' to insure that data is upload to redcap" 
-    slog.info(issue_label, issue_title, cmd = " ".join(sys.argv), info = infoTxt, **kwargs) 
+    
+    slog.info(issue_label + "-" + hashlib.sha1(infile).hexdigest()[0:6], issue_title, cmd = " ".join(sys.argv), info = infoTxt, **kwargs) 
   
     sys.exit(1)
 
