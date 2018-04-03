1bd25a3a1667d6f82f9bad264ce5887007ac204e
Simon Podhajsky <simon.podhajsky@gmail.com>
2018-04-02 19:52:07 -0700

    New script: Merge many datadict changes into the existing datadict

---
+++ datadict/datadict_utils.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def load_datadict(filepath, trim_index=True, trim_all=False):
+    df = pd.read_csv(filepath, index_col=0)
+    if trim_index:
+        df.index = df.index.str.strip()
+    if trim_all:
+        df = df.applymap(lambda x: x.strip() if type(x) is str else x)
+    return df
+
+def insert_rows_at(main_df, index_name, inserted_df, insert_before=False):
+    # Not checking if index exists because that will be apparent from error
+    # NOTE: This will not work with duplicate indices
+    pre_df = main_df.loc[:index_name]
+    post_df = main_df.loc[index_name:]
+    # Both pre_ and post_ contain the value at index_name, so one needs to
+    # drop it
+    if not insert_before:
+        pre_df = pre_df.drop(index_name)
+    else:
+        post_df = post_df.drop(index_name)
+    return pd.concat([pre_df, inserted_df, post_df],
+                     axis=0)
