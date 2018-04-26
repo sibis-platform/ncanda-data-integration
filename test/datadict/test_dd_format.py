af2ae1c0eae9d8cd5d1d201a4bea0b68b08df0f7
Simon Podhajsky <simon.podhajsky@gmail.com>
2018-04-25 19:39:56 -0700

    Test datadict_utils.insert_row_at
    (This is a first attempt at pytest.)

---
+++ test/datadict/test_dd_format.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import pytest
+from datadict.datadict_utils import *
+
+@pytest.fixture
+def datadict_df():
+    df = pd.DataFrame.from_dict({
+        "field_name": ["test1", "test2", "test3"],
+        "form_name": ["form1", "form1", "form2"],
+    })
+    return df.set_index("field_name")
+
+@pytest.fixture
+def single_insert_df():
+    df = pd.DataFrame.from_dict({
+        "field_name": ["test4"],
+        "form_name":  ["form1"]
+    })
+    return df.set_index("field_name")
+
+def test_insert_rows_at():
+    #result = insert_rows_at(datadict_df(), "test1", single_insert_df(), insert_before=True)
+    result = insert_rows_at(datadict_df(), "test1", single_insert_df())
+    assert result.index.tolist() == ["test4", "test1", "test2", "test3"]
