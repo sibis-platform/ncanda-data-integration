afb1cc65098e17a2cfd92b6aef33e6d99067dcb4
Simon Podhajsky <simon.podhajsky@gmail.com>
2018-09-28 17:28:03 -0700

    Prevent argname collision with kwargs

--- scripts/import/laptops/convert_util.py
+++ scripts/import/laptops/convert_util.py
@@ -6,9 +6,13 @@ from sibispy import sibislogger as slog
 def post_issue_and_exit(script, infile, verbose, post_to_github, issue_label, issue_title, **kwargs) : 
     slog.init_log(verbose, post_to_github,'NCANDA Import-Laptop: ' + script + ' Message', script)
 
-    infoTxt="After this issue is resolved please run 'harvester --file-to-upload " + infile + " --overwrite' to insure that data is upload to redcap" 
+    if 'post_resolution_instructions' not in kwargs:
+        infoTxt="After this issue is resolved please run 'harvester --file-to-upload " + infile + " --overwrite' to insure that data is upload to redcap" 
+    else:
+        infoTxt = kwargs['post_resolution_instructions']
+        del kwargs['post_resolution_instructions']
     
-    slog.info(issue_label + "-" + hashlib.sha1(infile).hexdigest()[0:6], issue_title, cmd = " ".join(sys.argv), info = infoTxt, **kwargs) 
+    slog.info(issue_label + "-" + hashlib.sha1(infile).hexdigest()[0:6], issue_title, cmd = " ".join(sys.argv), post_resolution_instructions = infoTxt, **kwargs) 
   
     sys.exit(1)
 
