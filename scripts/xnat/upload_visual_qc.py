2814ad35381d20d52ba77715b9b1dfc3631d11aa
kipohl <pohl.kilian@gmail.com>
2018-10-01 09:16:45 -0700

    Modified upload_visual_qc so it can be sourced and run by itself

--- scripts/xnat/upload_visual_qc.py
+++ scripts/xnat/upload_visual_qc.py
@@ -5,44 +5,73 @@
 ##  for the copyright and license terms
 ##
 
-import argparse
 import pandas as pd
 import sibispy
 from sibispy import sibislogger as slog
+import os
 
-# example input /fs/ncanda-share/log/check_new_sessions/scans_to_review.csv
-parser = argparse.ArgumentParser(description="Uploads qc results to XNAT" )
-parser.add_argument("qc_csv_file",help="csv file with qc results",action="store")
-args = parser.parse_args()
+def upload_findings_to_xnat(sibis_session,qc_csv_file, sendEmailFlag): 
+    if not os.path.exists(qc_csv_file):
+        raise IOError("Please ensure {} exists!".format(qc_csv_file))
 
-##############
-# INPUT DATA
-f=pd.read_csv(args.qc_csv_file)
+    ##############
+    # INPUT DATA
+    try: 
+       fData=pd.read_csv(qc_csv_file)
+    except Exception as err_msg :
+       raise IOError("Reading of File {} failed with the following error message: {}".format(qc_csv_file,str(err_msg)))
+ 
+    questionable_scans="" 
+    for index,row in fData.iterrows():
 
-
-###########
-# SESSION
-slog.init_log(False, False,'upload_visual_qc', 'upload_visual_qc','/tmp')
-session = sibispy.Session()
-session.configure()
-server = session.connect_server('xnat', True)
-for index,row in f.iterrows():
-        exp=session.xnat_get_experiment(row['xnat_experiment_id'])
-        sc=exp.scan(str(row['scan_id'])).attrs
+        exp=sibis_session.xnat_get_experiment(row['xnat_experiment_id'])
+        scan=exp.scan(str(row['scan_id'])).attrs
 
         # quality
         if row['decision']==1:
-                sc.set('quality','usable')
+                scan.set('quality','usable')
         elif row['decision']==0:
-                sc.set('quality','questionable')
+                scan.set('quality','questionable')
+                questionable_scans +=  row['xnat_experiment_id'] + " " + str(row['scan_id']);
+                if isinstance(row['scan_note'],str) and len(row['scan_note'])>0:
+                        questionable_scans +=  " " + row['scan_note']
+
+                questionable_scans +=  " " + str(os.path.join(row['nifti_folder'],str(row['scan_id']) + "_" + str(row['scan_type']), "*.gz")) +  "<BR>" 
         else:
-                sc.set('quality','unknown')
-
-#        elif row['decision']==-1:
-#                sc.set('quality','unusable')
-
+                scan.set('quality','unknown')
 
         # comment
-        if len(row['scan_note'])>0:
-		sc.set('note',row['scan_note'])
+        if isinstance(row['scan_note'],str) and len(row['scan_note'])>0:
+		scan.set('note',row['scan_note'])
+
+    # send email to qc manager
+    if sendEmailFlag and len(questionable_scans) :
+        from sibispy import sibis_email 
+        email = sibis_email.xnat_email(sibis_session)
+
+        email.send('%s XNAT - Questionable Scans' % sibis_session.get_project_name(),
+                   email._admin_email,
+                   email._admin_email,
+                   str(questionable_scans),
+                   False)    
+
+#============================================================
+# Main
+#============================================================
+if __name__ == "__main__": 
+    import argparse
+    # example input /fs/ncanda-share/log/check_new_sessions/scans_to_review.csv
+    parser = argparse.ArgumentParser(description="Uploads qc results to XNAT" )
+    parser.add_argument("qc_csv_file",help="csv file with qc results",action="store")
+    args = parser.parse_args()
+
+    ###########
+    # SESSION
+    slog.init_log(False, False,'upload_visual_qc', 'upload_visual_qc','/tmp')
+    session = sibispy.Session()
+    session.configure()
+    server = session.connect_server('xnat', True)
+
+    upload_findings_to_xnat(session,args.qc_csv_file, True)
+
 
