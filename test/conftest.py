49d12615510a1a125edd1fba2cf86d005bc7f329
Jim Klo <jim.klo@sri.com>
2018-11-01 17:45:21 -0700

    Add pytest configuration
    redirect the cache dir to somewhere writeable
    add some default fixtures for config file and cluster jobs

---
+++ test/conftest.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, print_function
+
+import os, yaml
+
+def pytest_addoption(parser):
+  parser.addoption("--config-file", action="store", default=os.path.expanduser("~/.sibis-general-config.yml"), help="Path to SIBIS General Configuration File")
+  parser.addoption("--cluster-job", action="store_true", default=False, help="Running as cluster job")
+
+def pytest_generate_tests(metafunc):
+  option_value = metafunc.config.option.config_file
+  print("opt_val: >{0}<".format(option_value))
+  general_cfg = yaml.load(option_value)
+  if 'config' in metafunc.fixturenames and general_cfg is not None:
+    metafunc.parametrize("config", [general_cfg])
+  if 'config_file' in metafunc.fixturenames and general_cfg is not None:
+    metafunc.parametrize("config_file", [option_value])
+
+  option_value = metafunc.config.option.cluster_job
+  if 'cluster_job' in metafunc.fixturenames and option_value is not None:
+    metafunc.parametrize("cluster_job", [option_value])
