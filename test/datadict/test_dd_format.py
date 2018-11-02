bd375cefa2bdb91efb04163a5e7e810ee1e01eb9
Jim Klo <jim.klo@sri.com>
2018-11-01 17:43:52 -0700

    fix existing pytest

--- test/datadict/test_dd_format.py
+++ test/datadict/test_dd_format.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 import pandas as pd
 import pytest
 from datadict.datadict_utils import *
@@ -18,7 +20,7 @@ def single_insert_df():
     })
     return df.set_index("field_name")
 
-def test_insert_rows_at():
+def test_insert_rows_at(datadict_df, single_insert_df):
     #result = insert_rows_at(datadict_df(), "test1", single_insert_df(), insert_before=True)
-    result = insert_rows_at(datadict_df(), "test1", single_insert_df())
+    result = insert_rows_at(datadict_df, "test1", single_insert_df)
     assert result.index.tolist() == ["test4", "test1", "test2", "test3"]
