9a87a2af732b5010151c85265946b5f639b7612d
Simon Podhajsky <simon.podhajsky@sri.com>
2019-05-06 16:53:06 -0700

    Factor out commonly used QA/reporting functions

--- scripts/qc/load_utils.py
+++ scripts/qc/load_utils.py
@@ -6,7 +6,7 @@ import redcap as rc
 def chunked_export(project, form, chunk_size=100, verbose=True, **kwargs):
     def chunks(l, n):
         """Yield successive n-sized chunks from list l"""
-        for i in xrange(0, len(l), n):
+        for i in range(0, len(l), n):
             yield l[i:i+n]
     record_list = project.export_records(fields=[project.def_field])
     records = [r[project.def_field] for r in record_list]
@@ -38,6 +38,63 @@ def load_all_forms(api, arm='1'):
     form_n = len(forms)
     dfs = []
     for i, form in enumerate(forms):
-        print i, form_n, form
+        print(i, form_n, form)
         dfs.append(chunked_export(api, form))
     return pd.concat(dfs, axis=1)
+
+
+def all_forms_exist_in_redcap(form_names, api):
+    meta = api.export_metadata(format='df')
+    available_forms = meta['form_name'].unique().tolist()
+    for form in form_names:
+        if form not in available_forms:
+            return False
+    return True
+
+
+def load_form(api, form_name, verbose=True):
+    if verbose:
+        print(form_name)
+    
+    try:
+        if verbose:
+            print("Trying chunked export, 5000 records at a time")
+        return chunked_export(api, form_name, 5000)
+    except (ValueError, rc.RedcapError, pd.io.common.EmptyDataError):
+        pass
+    
+    # 2. Chunked load with chunk size of 1000
+    try:
+        if verbose:
+            print("Trying chunked export, 1000 records at a time")
+        return chunked_export(api, form_name, 1000)
+    except (ValueError, rc.RedcapError, pd.io.common.EmptyDataError):
+        pass
+    
+    # 2. Chunked load with default chunk size
+    try:
+        if verbose:
+            print("Trying chunked export, default chunk size (100)")
+        return chunked_export(api, form_name, 100)
+    except (ValueError, rc.RedcapError, pd.io.common.EmptyDataError):
+        pass
+    
+    # 3. Chunked load with tiny chunk
+    try:
+        if verbose:
+            print("Trying chunked export with tiny chunks (10)")
+        return chunked_export(api, form_name, 10)
+    except (ValueError, rc.RedcapError, pd.io.common.EmptyDataError):
+        print("Giving up")
+        return None
+
+
+def load_form_with_primary_key(api, form_name, verbose=True):
+    df = load_form(api, form_name, verbose)
+    if df is not None:
+        if api.def_field in df.index.names:
+            return df
+        if api.is_longitudinal():
+            return df.set_index([api.def_field, "redcap_event_name"])
+        else:
+            return df.set_index([api.def_field])
--- scripts/qc/qa_utils.py
+++ scripts/qc/qa_utils.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
+
+"""
+## FIXME
+
+- There's multiple functions doing mostly the same thing, but not quite. I need to go through it.
+"""
+
 def get_items_matching_regex(regex, haystack):
     import re
-    return filter(lambda x: re.search(regex, x), haystack)
+    return list(filter(lambda x: re.search(regex, x), haystack))
 
 
 def get_notnull_entries(row, ignore_always_notna=True):
@@ -12,17 +19,17 @@ def get_notnull_entries(row, ignore_always_notna=True):
     except:
         columns = row.index.tolist()
     if ignore_always_notna:
-        cols_complete = get_items_matching_regex("complete$", columns)
+        cols_complete = get_items_matching_regex("_complete$", columns)
         cols_ignore = get_items_matching_regex("^visit_ignore", columns)
-        cols_missing = get_items_matching_regex("missing(_why(_other)?)?$", 
+        cols_missing = get_items_matching_regex("_missing(_why(_other)?)?$", 
                                                 columns)
-        cols_clinical = get_items_matching_regex("^fh_|^ssq_", columns)
+        #cols_clinical = get_items_matching_regex("^fh_|^ssq_", columns)
         # cols_missing_rationale = get_items_matching_regex("missing_why(_other)?$",
         #                                                   columns)
         cols_checklist = get_items_matching_regex("___", columns)
 
         non_nan_items = row.drop(cols_complete + cols_ignore + cols_missing +
-                                 cols_clinical +  # cols_missing_rationale +
+                                 #cols_clinical +  # cols_missing_rationale +
                                  cols_checklist).notnull()
     else:
         non_nan_items = row.notnull()
@@ -30,10 +37,118 @@ def get_notnull_entries(row, ignore_always_notna=True):
     return non_nan_items
 
 
+def count_non_nan_rowwise(df, form_name=None, drop_column=None):
+    """ A more efficient method of checking non-NaN values """
+    # 1. check complete
+    if form_name:
+        complete_field = form_name + '_complete'
+        if drop_columns:
+            drop_columns.append(complete_field)
+        else:
+            drop_columns = [complete_field]
+    if drop_columns is None:
+        drop_columns = []
+    
+    # 2. count up NaNs
+    return df.drop(drop_columns, axis=1).notnull().sum(axis=1)
+
+
 def count_notnull_entries(row):
-    return get_notnull_entries(row).sum(axis=1)
+    try:
+        return get_notnull_entries(row).sum(axis=1)
+    except ValueError:
+        return get_notnull_entries(row).sum()
 
 
 def has_notnull_entries(row):
     return get_notnull_entries(row).any()
 
+
+def form_has_content(row):
+    """ If the form is knowledgeably not empty (e.g. marked missing or 
+    marked complete) or it has content in it, it is considered to have content. """
+    try:
+        columns = row.columns.tolist()
+    except:
+        columns = row.index.tolist()
+    cols_complete = get_items_matching_regex("complete$", columns)
+    cols_missing  = get_items_matching_regex("missing$", columns)
+    cols_checklist = get_items_matching_regex("___", columns)
+    
+    if cols_missing:
+        missing = (row[cols_missing] == 1).any()
+    else:
+        missing = None
+    
+    if cols_complete:
+        complete = (row[cols_complete].isin([2])).any()
+    else:
+        complete = False
+    
+    non_nan_items = row.drop(cols_complete + cols_missing + cols_checklist).notnull().any()
+    
+    return missing | complete | non_nan_items
+
+
+def form_has_content_and_is_not_missing(row):
+    """ If the form is *not* marked missing *and* has any non-empty non-meta 
+    fields (which count_notnull_entries gets), then it's considered to have content.
+    """
+    try:
+        columns = row.columns.tolist()
+    except:
+        columns = row.index.tolist()
+
+    cols_missing  = get_items_matching_regex("missing$", columns)
+    if cols_missing:
+        missing = (row[cols_missing] == 1).any()
+    else:
+        missing = None
+       
+    notnull_count = count_notnull_entries(row)
+    
+    return (not missing) and (notnull_count > 0)
+
+# Taken from http://pycap.readthedocs.io/en/latest/deep.html#dealing-with-large-exports
+# and adapted to scope down to forms
+
+# FIXME: Possibly duplicates chunk edges? Need to check it out
+def chunked_form_export(project, forms, events=None, include_dag=False, chunk_size=100):
+    if isinstance(forms, str):
+        forms = [form]
+    if isinstance(events, str):
+        events = [events]
+        
+    def chunks(l, n):
+        """Yield successive n-sized chunks from list l"""
+        for i in range(0, len(l), n):
+            yield l[i:i+n]
+    record_list = project.export_records(fields=[project.def_field])
+    records = [r[project.def_field] for r in record_list]
+    try:
+        response = None
+        record_count = 0
+        for record_chunk in chunks(records, chunk_size):
+            record_count = record_count + chunk_size
+            #print record_count
+            chunked_response = project.export_records(records=record_chunk, 
+                                                      fields=[project.def_field],
+                                                      forms=forms,
+                                                      events=events,
+                                                      export_data_access_groups=include_dag,
+                                                      format='df',
+                                                      df_kwargs={'low_memory': False})
+            if response is not None:
+                response = pd.concat([response, chunked_response], axis=0)
+            else:
+                response = chunked_response
+    except rc.RedcapError:
+        msg = "Chunked export failed for chunk_size={:d}".format(chunk_size)
+        raise ValueError(msg)
+    else:
+        if project.is_longitudinal:
+            response.set_index([project.def_field, 'redcap_event_name'], inplace=True)
+        else:
+            response.set_index([project.def_field], inplace=True)
+            
+        return response
