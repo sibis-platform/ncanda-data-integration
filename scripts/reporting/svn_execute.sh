e094f6ae8db6ca9e684686ffce8dbcc9e6d938d8
cpondoc <chris.pondoc@comcast.net>
2021-08-31 09:44:49 -0700

    SVN Reports now working within ncanda-data-integration

--- scripts/reporting/svn_execute.sh
+++ scripts/reporting/svn_execute.sh
@@ -2,6 +2,8 @@
 # Script to programmatically execute all of the SVN Reports using Bash!
 
 # Directories for: SVN script, directory for dashboards, and directory for csvs
+DASHBOARDS_DIR=/fs/ncanda-share/beta/chris/ncanda-data-integration/scripts/dashboards
+SVN_FILE='SVN Reports.ipynb'
 SCRIPT_DIR=/fs/ncanda-share/beta/chris/ncanda-data-integration/scripts/reporting
 SAVE_DIR=/fs/ncanda-share/log/status_reports/sla_dashboards
 STATUS_DIR=/fs/ncanda-share/log/status_reports/sla_files
@@ -22,6 +24,6 @@ popd
 
 # For each site -- generate papermill notebook, then convert to HTML
 for site in ${sites[@]}; do
-    papermill 'notebooks/SVN Reports.ipynb' $SAVE_DIR/$site.ipynb -p site $site
+    papermill '/fs/ncanda-share/beta/chris/ncanda-data-integration/scripts/dashboards/SVN Reports.ipynb' $SAVE_DIR/$site.ipynb -p site $site
     jupyter nbconvert --to html $SAVE_DIR/$site.ipynb --TagRemovePreprocessor.remove_cell_tags='{"remove_cell"}'
 done
