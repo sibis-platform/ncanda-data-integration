6a7cb8a6b548583472d9d5d7064106ba4c590857
cpondoc <chris.pondoc@comcast.net>
2021-09-02 14:26:23 -0700

    Create new script to generate all REDCap Forms and edit JSON file to add additional column name

---
+++ scripts/dashboards/all_redcap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -i
+# Script to programmatically generate All REDCap Forms Dashboards
+
+# Paths for: Dashboard, Save Directory, and Inventory Data
+DASHBOARD_FILE="/fs/ncanda-share/beta/chris/ncanda-data-integration/scripts/dashboards/AllRedcapForms.ipynb"
+SAVE_DIR="/fs/ncanda-share/log/status_reports/all_redcap_dashboards"
+DATA_DIR="/fs/ncanda-share/log/make_all_inventories/inventory_by_site"
+
+# First, generate Inventory Dates
+#python3 /fs/ncanda-share/beta/chris/
+
+# Loop through sites
+pushd $DATA_DIR
+for site in *; do
+    mkdir $SAVE_DIR/$site
+    pushd $site
+    # And then through events...
+    for event in *; do
+	papermill $DASHBOARD_FILE $SAVE_DIR/$site/$event.ipynb -p site $site -p arm $event
+	jupyter nbconvert --to html $SAVE_DIR/$site/$event.ipynb --TagRemovePreprocessor.remove_cell_tags='{"remove_cell"}'
+	#echo $SAVE_DIR/$site/$event.ipynb
+    done
+    popd
+done
+popd
+
