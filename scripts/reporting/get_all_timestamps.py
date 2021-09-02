0a3196fc8471dbc5f5c618c9156cafc56c1041b4
cpondoc <chris.pondoc@comcast.net>
2021-09-02 09:13:04 -0700

    Finish up get_all_timestamps.py

--- scripts/reporting/get_all_timestamps.py
+++ scripts/reporting/get_all_timestamps.py
@@ -4,27 +4,30 @@ Gets timestamps for the REDCap All Forms Dashboard
 import sibispy
 import sys
 
-'''
-All arms necessary for getting all the dates
-'''
-arms = ['18month_followup_arm_1', '4y_visit_arm_1', '78month_followup_arm_1', '1y_visit_arm_1', '54month_followup_arm_1', '7y_visit_arm_1', '2y_visit_arm_1', '5y_visit_arm_1', '8y_visit_arm_1', '30month_followup_arm_1', '66month_followup_arm_1', '90month_followup_arm_1', '3y_visit_arm_1', '6month_followup_arm_1', 'baseline_visit_arm_1', '42month_followup_arm_1', '6y_visit_arm_1']
-
-#arms=['7y_visit_arm_1']
-
+# Path to store timestamp csv files
 PATH='/fs/ncanda-share/log/status_reports/inventory_dates/'
 
 def main():
     '''
     Sets up and configures sessions, passes in to get al correspoding .csv files
     '''
+    events = []
+    # Connect to a Sibispy Session
     session = sibispy.Session()
     if not session.configure():
         raise RuntimeError("sibispy is not configured; aborting!")
         sys.exit(1)
     api = session.connect_server('data_entry')
-    for arm in arms:
-        print(arm)
-        data_entry = api.export_records(format='df', fields=['visit_date'], events=[arm])
-        data_entry = data_entry.replace('', "2001-10-15", regex=True)
-        data_entry.to_csv(PATH + arm + '.csv')
-main()
+
+    # Get the data, add event name as column, and generate corresponding events
+    data_entry = api.export_records(format='df', fields=['visit_date'])
+    data_entry['redcap_name'] = data_entry.index.get_level_values('redcap_event_name')
+    events = data_entry.index.get_level_values('redcap_event_name').unique().tolist()
+    
+    # Iterate through each event, find all columns with event name, and create CSV
+    for event in events:
+        event_subset = data_entry[data_entry['redcap_name'] == event].drop(columns=['redcap_name'])
+        event_subset.to_csv(PATH + event + '.csv')
+        
+if __name__ == "__main__":
+    main()
