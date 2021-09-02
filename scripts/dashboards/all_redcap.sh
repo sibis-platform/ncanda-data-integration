81fe72eb7567e44103490641151299fe31f1c2e3
cpondoc <chris.pondoc@comcast.net>
2021-09-02 14:33:31 -0700

    Finalize scripts, move to correct file, and generate all timestamps before hand

--- scripts/dashboards/all_redcap.sh
+++ scripts/dashboards/all_redcap.sh
@@ -5,9 +5,10 @@
 DASHBOARD_FILE="/fs/ncanda-share/beta/chris/ncanda-data-integration/scripts/dashboards/AllRedcapForms.ipynb"
 SAVE_DIR="/fs/ncanda-share/log/status_reports/all_redcap_dashboards"
 DATA_DIR="/fs/ncanda-share/log/make_all_inventories/inventory_by_site"
+TIMESTAMPS_FILE="/fs/ncanda-share/beta/chris/ncanda-data-integration/scripts/reporting/get_all_timestamps.py"
 
 # First, generate Inventory Dates
-#python3 /fs/ncanda-share/beta/chris/
+python3 $TIMESTAMPS_FILE
 
 # Loop through sites
 pushd $DATA_DIR
