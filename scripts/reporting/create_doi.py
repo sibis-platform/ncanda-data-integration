53245fa90a71d5371b1086a972037d2ea175ae0e
cpondoc <chris.pondoc@comcast.net>
2021-09-10 08:36:04 -0700

    Create successful POST request

--- scripts/reporting/create_doi.py
+++ scripts/reporting/create_doi.py
@@ -1,11 +1,15 @@
 '''
 Programmatically create DOI for a Synapse project
 '''
+import json
 import os
 import requests
 import synapseclient
 from synapseclient import Project, Folder, File
 import yaml
+
+''' Constants '''
+OBJECT_ID = 'syn25191261'
 
 def synapse_login():
     '''
@@ -18,11 +22,30 @@ def synapse_login():
               password=config.get('synapse').get('password'))
     return syn
 
+def get_entity_properties(syn, object_id):
+    '''
+    Get properties of current Synapse object, given current Synapse ID
+    '''
+    entity = syn.get(object_id)
+    return entity.properties
+
+def create_doi(syn_client, file_path):
+    '''
+    Read in JSON from file and generate DOI programmatically
+    '''
+    f = open(file_path, 'r')
+    test_send_body = json.load(f)
+    send_body = json.dumps(test_send_body)
+    response = syn_client.restPOST('https://repo-prod.prod.sagebase.org/repo/v1/doi/async/start', send_body)
+    return response
+
 def main():
     '''
     Set up synapse client, connect to correct info, etc.
     '''
     syn_client = synapse_login()
+    initial_props = get_entity_properties(syn_client, OBJECT_ID)
+    token_number = create_doi(syn_client, 'test_doi.json')
 
 if __name__ == '__main__':
     main()
