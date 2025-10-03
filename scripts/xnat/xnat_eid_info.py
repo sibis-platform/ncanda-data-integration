4245afa9f9b7849ddf2b996a4231401a43d71ce2
kipohl <pohl.kilian@gmail.com>
2025-10-03 16:14:19 -0700

    Expanded it so it could be looked up by label instead of eid

--- scripts/xnat/xnat_eid_info.py
+++ scripts/xnat/xnat_eid_info.py
@@ -19,14 +19,16 @@ def init_session() :
     session.connect_server('xnat', True)
     return session
 
-def eid_info(sibis_session,eid,verbose=False) :
-    exp=sibis_session.xnat_get_experiment(eid)
+def eid_info(sibis_session,id,label, verbose=False) :
+
+    exp=sibis_session.xnat_get_experiment(id)
+        
     if exp is None:
         if verbose :
-            print("ERROR:No record found for",eid)
-        print(eid+",,,")    
+            print("ERROR:No record found for",iid)
+        print(id+",,,")    
         return 1
-    print(','.join([eid,exp.label,exp.subject_id,exp.project]))
+    print(','.join([exp.id,exp.label,exp.subject_id,exp.project]))
     return 0
 
 #============================================================
@@ -35,12 +37,12 @@ def eid_info(sibis_session,eid,verbose=False) :
 if __name__ == "__main__": 
     import argparse
     # example NCANDA_E12152
-    parser = argparse.ArgumentParser(description="Lookup info related to experiment with eid" )
-    parser.add_argument("eid",help="eid",action="store")
-    parser.add_argument("-v", "--verbose", help="Verbose operation", action="store_true")
+    parser = argparse.ArgumentParser(description="Lookup info related to experiment" )
+    parser.add_argument("id",help="eid  (NCANDA_E00001) or label (e.g. E-00XXX-F-4-20250720)",action="store")
+    parser.add_argument("-v", "--verbose", help="Verbose operation", action="store_true")    
     args = parser.parse_args()
 
     sibis_session=init_session()
-    sys.exit(eid_info(sibis_session,args.eid, args.verbose))
+    sys.exit(eid_info(sibis_session,args.id, args.verbose))
 
 
